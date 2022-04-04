9fd7de16c02333de6d507a5173241468c12b557e
Futurity <vocalmusicstudio20@gmail.com>
2022-04-04 13:39:29 +0000

    update server

--- server.js
+++ server.js
@@ -2,5 +2,6 @@ const http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end(`Ola Terceiro B mais querido! \n\n` + dataTime.myDateTime());
+  res.write(req.url)
+  res.end()
 }).listen(8080);

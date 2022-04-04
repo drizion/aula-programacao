058ac1ba0c7b63333e6821e7c1593ff0d117e52d
Futurity <vocalmusicstudio20@gmail.com>
2022-04-04 13:36:44 +0000

    remove my_modules

--- my_modules/calculator.js
+++
@@ -1,3 +0,0 @@
-const calculator = require("./operator")
-
-console.log(calculator.divide(10,5))
--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const http = require('http');
-const dataTime = require('./my_modules/time')
-const calculator = require('./my_modules/calculator')
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});

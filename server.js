d022595d608d10fac09e1e8b114d74ab75a86b92
Futurity <vocalmusicstudio20@gmail.com>
2022-04-04 14:00:10 +0000

    url split tchupaq tchupleis e coisarada

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-const http = require('http');
+var http = require('http');
+var url = require('url');
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write(req.url)
-  res.end()
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  var q = url.parse(req.url, true).query;
+  var txt = q.year + " " + q.month;
+  res.end(txt);
 }).listen(8080);
